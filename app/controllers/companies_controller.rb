b81ab6e670d48b70695e7cc269607ec8af733a98
javpet <javorkai.peter@gmail.com>
2018-04-05 15:09:15 +0200

    Form implemented

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,10 +6,24 @@ class CompaniesController < ApplicationController
 
   def new
     # Showing them the form
+    @company = Company.new
   end
 
   def create
     # Adding new company records to the db
+    @company = Company.new(form_params)
+
+    if @company.save
+      flash[:success] = "Thanks for sending us another great company. We review every application before we put them online!"
+
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  def form_params
+    params.require(:company).permit(:company_name, :company_url, :intern_payment, :sender_name, :sender_email)
   end
 
 end
